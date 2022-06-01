e7a0fa87c6a92f4d0c658f8d798cd620225b969b
mohamed sulaiman <mohamed@outlook.sa>
2022-06-02 01:31:49 +0300

    Added files the lsb

---
+++ BlogLab/BlogApp/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    title= models.CharField(max_length=255)
+    content = models.TextField(null=True)
+    date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='images')
+    
+    def __str__(self) -> str:
+        return self.title
+
+
+class Comment(models.Model):
+    first_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    content = models.TextField(null=True)
+    date = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return self.first_name
